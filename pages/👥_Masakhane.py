800341046014c17996c121176c829e0772f115e6
Benjamin Gross <ben@benbox.org>
2023-02-02 13:43:35 +0200

    added page `👥_Masakhane.py` with library `google.colab`

---
+++ pages/👥_Masakhane.py
@@ -0,0 +1,7 @@
+##### `👥_Masakhane.py`
+##### OpenAI ChatGPT Demo
+##### Open-Source, hostet on https://github.com/DrBenjamin/OpenAI
+##### Please reach out to ben@benbox.org for any questions
+#### Loading needed Python libraries
+from google.colab import drive
+drive.mount('/content/drive')
