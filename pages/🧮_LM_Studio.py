ac1d35421888cff3fafe24bbb7efb2b5aeab5c8a
DrBenjamin <ben@benbox.org>
2024-06-10 13:35:32 +0200

    improvements

--- pages/🧮_LM_Studio.py
+++ pages/🧮_LM_Studio.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from openai import OpenAI
 # Pointing to the local server
-client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+client = OpenAI(base_url="http://192.168.178.24:1234/v1", api_key="lm-studio")
 
-completion = client.chat.completions.create(
-  model="LM Studio Community/Meta-Llama-3-8B-Instruct-GGUF",
-  messages=[
-    {"role": "system", "content": "Always answer in rhymes."},
-    {"role": "user", "content": "Introduce yourself."}
-  ],
-  temperature=0.7,
-)
+prompt = ""
+prompt = st.text_input("Prompt")
+if prompt != "":
+  completion = client.chat.completions.create(
+    model="LM Studio Community/Meta-Llama-3-8B-Instruct-GGUF",
+    messages=[
+      {"role": "system", "content": "You are a friendly chat bot."},
+      {"role": "user", "content": prompt}
+    ],
+    temperature=0.7,
+  )
 
-st.write(completion.choices[0].message)
+  st.write(completion.choices[0].message.content)
