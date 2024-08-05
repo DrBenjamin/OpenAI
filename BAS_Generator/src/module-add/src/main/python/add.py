c12352c3ed3bd595559e2bd0dd1ecf1ca94ac035
DrBenjamin <ben@benbox.org>
2024-08-06 00:06:58 +0200

    added libraries

--- BAS_Generator/src/module-add/src/main/python/add.py
+++ BAS_Generator/src/module-add/src/main/python/add.py
@@ -1,8 +1,11 @@
 # This is where you can create python functions, which can further
 # be used to create Snowpark UDFs and Stored Procedures in your setup_script.sql file.
-
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import lit
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI
 
 # UDF example:
 def add_fn(x: int, y: int) -> int:
