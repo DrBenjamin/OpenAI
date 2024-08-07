11582880d28cecf1b7bf310f4137580334208a74
DrBenjamin <ben@benbox.org>
2024-08-07 10:10:46 +0200

    minor changes

--- BAS_Generator/src/module-add/src/main/python/add.py
+++ BAS_Generator/src/module-add/src/main/python/add.py
@@ -2,21 +2,16 @@
 # be used to create Snowpark UDFs and Stored Procedures in your setup_script.sql file.
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import lit
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_openai import ChatOpenAI
+#from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+#from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+#from langchain_core.runnables.history import RunnableWithMessageHistory
+#from langchain_openai import ChatOpenAI
 
 # UDF example:
 def add_fn(x: int, y: int) -> int:
     return x + y
 
-
 # Stored Procedure example:
 def increment_by_one_fn(session: Session, x: int) -> int:
     df = session.create_dataframe([[]]).select((lit(1) + lit(x)).as_('RESULT'))
     return df.collect()[0]['RESULT']
-  
-# Chatbot example:
-def chatbot_fn(session: Session, chatbot: ChatOpenAI, prompt: str) -> str:
-    return chatbot.get_response(prompt)
