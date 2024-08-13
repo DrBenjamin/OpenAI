fc446c0c5957360563aa845f33ecb2c186a0c41f
DrBenjamin <ben@benbox.org>
2024-08-13 12:22:40 +0200

    added web scraping

--- pages/❄️_BAS_Anzeigen_Generator.py
+++ pages/❄️_BAS_Anzeigen_Generator.py
@@ -5,6 +5,8 @@
 import streamlit as st
 import pandas as pd
 import sys
+import requests
+from bs4 import BeautifulSoup
 from snowflake.snowpark import Session
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
@@ -51,20 +53,28 @@ with st.expander("Datenbankinhalt"):
 # Sidebar
 sidebar = st.sidebar
 with sidebar:
-  kunde = st.text_input("Kunde:", value="GWQ ServicePlus AG")
-  cloud = st.selectbox("Cloud:", ["AWS", "Azure", "Google Cloud"], index=2)
-  on = st.toggle("OpenAI ChatGPT", True)
-  system = st.text_input("System:", value = f"Du erstellst einzelne Absätze einer Anzeige beim Bundesamt für Soziale Sicherung über die Verarbeitung von Sozialdaten im Auftrag (AVV) nach § 80 Zehntes Sozialgesetzbuch (SGB X). Tausche <Variabel_Name> durch die entsprechenden Inhalte aus und gebe nur den Text aus und verzichte auf Phrasen wie z.B. 'Vielen Dank für die Informationen. Hier sind die angepassten Absätze für die Anzeige beim Bundesamt für Soziale Sicherung:'.")
-  if not on:
-    st.markdown("Local Server Configuration")
-    url = st.text_input("URL:", value="http://localhost")
-    port = st.number_input("Port:", value=1234, min_value=1, max_value=65535)
+    kunde = st.text_input("Kunde:", value="GWQ ServicePlus AG")
+    web = st.toggle("Webscraper", True)
+    kunde_url = st.text_input("Kunde Info:", value="https://www.gwq-serviceplus.de/ueber-uns")
+    if web:
+        kunde_page = requests.get(kunde_url)
+        kunde_soup = BeautifulSoup(kunde_page.content, 'html.parser')
+        kunde_info = kunde_soup.get_text()
+        kunde_info = kunde_info.replace('\n', ' ')
+    cloud = st.selectbox("Cloud:", ["AWS", "Azure", "Google Cloud"], index=2)
+    on = st.toggle("OpenAI ChatGPT", True)
+    system = st.text_input("System:", value = f"Du erstellst einzelne Absätze einer Anzeige beim Bundesamt für Soziale Sicherung über die Verarbeitung von Sozialdaten im Auftrag (AVV) nach § 80 Zehntes Sozialgesetzbuch (SGB X). Tausche die Platzhalter (z.B. <Kunde>) durch die entsprechenden Inhalte aus und gebe nur den verbesserten Text in einer sachlichen und formellen Form aus und verzichte auf Phrasen wie z.B. 'Vielen Dank für die Informationen. Hier sind die angepassten Absätze für die Anzeige beim Bundesamt für Soziale Sicherung:'.")
+    if not on:
+      st.markdown("Local Server Configuration")
+      url = st.text_input("URL:", value="http://localhost")
+      port = st.number_input("Port:", value=1234, min_value=1, max_value=65535)
 
 # Set up memory
 msgs = StreamlitChatMessageHistory(key="langchain_messages")
 if len(msgs.messages) == 0:
-    msgs.add_ai_message(f"""Ich ersetze <Kunde> mit {kunde}, 
-                          <Cloud-Anbieter> mit {cloud}.""")
+    msgs.add_ai_message(f"""Ich schreibe den Text in einer sachlichen und formellen
+                            Form um und ersetze <Kunde> mit {kunde}, 
+                            <Cloud-Anbieter> mit {cloud}.""")
 
 view_messages = st.expander("Zeige mir die Paragraphen an")
 
@@ -79,18 +89,18 @@ prompt = ChatPromptTemplate.from_messages(
 
 # Setting the LLM
 if on:
-  chain = prompt | ChatOpenAI(
-    model="gpt-4o-mini",
-    api_key=st.secrets["openai"]["key"]
-  ) 
+    chain = prompt | ChatOpenAI(
+        model="gpt-4o-mini",
+        api_key=st.secrets["openai"]["key"]
+    )
 else:
-  server_url = f"{url}:{str(port)}/v1"
-  chain = prompt | ChatOpenAI(
-    base_url=server_url,
-    model="llama-3-8b-chat-doctor-Q4_K_M_v2",
-    temperature=0.5,
-    max_tokens=4000,
-    api_key="lm-studio"
+    server_url = f"{url}:{str(port)}/v1"
+    chain = prompt | ChatOpenAI(
+        base_url=server_url,
+        model="llama-3-8b-chat-doctor-Q4_K_M_v2",
+        temperature=0.5,
+        max_tokens=4000,
+        api_key="lm-studio"
   )
 
 chain_with_history = RunnableWithMessageHistory(
@@ -106,9 +116,15 @@ for msg in msgs.messages:
 
 # If user inputs a new prompt, generate and draw a new response
 for prompt in df["PARAGRAPH_TEXT"]:
+    #if '<Kundeninfo>' in paragraph_text and web:
+    #    prompt = paragraph_text.replace('<Kunde>', str(kunde)).replace('<Cloud-Anbieter>', str(cloud)).replace('<Kundeninfo>', str(kunde_info))
+    #else:
+    #    prompt = paragraph_text.replace('<Kunde>', str(kunde)).replace('<Cloud-Anbieter>', str(cloud))
     st.chat_message("human").write(prompt)
+    
     # Note: new messages are saved to history automatically by Langchain during run
     config = {"configurable": {"session_id": "any"}}
+    print(prompt)
     response = chain_with_history.invoke({"question": prompt}, config)
     st.chat_message("ai").write(response.content)
 
